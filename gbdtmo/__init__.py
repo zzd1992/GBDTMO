60e4c0556a96cec15204b57f51d2e881a94e71ae
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2021-09-19 22:56:58 +0200

    Add the wrapper to install with the package

--- gbdtmo/__init__.py
+++ gbdtmo/__init__.py
@@ -2,6 +2,7 @@ from .lib_utils import load_lib
 from .histogram import get_bins_maps
 from .gbdtmo import GBDTSingle, GBDTMulti
 from .plotting import create_graph
+from .wrapper import GBDTMO_classifier, GBDTMO_regression
 
-__all__ = ["load_lib", "create_graph", "get_bins_maps", "GBDTSingle", "GBDTMulti"]
-
+__all__ = ["load_lib", "create_graph", "get_bins_maps", "GBDTSingle",
+           "GBDTMulti", "GBDTMO_classifier", "GBDTMO_regression"]
