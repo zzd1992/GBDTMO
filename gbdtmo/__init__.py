3853464ede270bc866e1ef1d4f99fb5581606e9e
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2021-09-23 12:12:46 +0200

    Update __init__.py

--- gbdtmo/__init__.py
+++ gbdtmo/__init__.py
@@ -1,8 +1,9 @@
 from .lib_utils import load_lib
 from .histogram import get_bins_maps
-from .gbdtmo import GBDTSingle, GBDTMulti
+from .gbdtmo import GBDTSingle, GBDTMulti, GBDTMulti_regression, GBDTMulti_classification
 from .plotting import create_graph
-from .wrapper import GBDTMO_classifier, GBDTMO_regression
 
 __all__ = ["load_lib", "create_graph", "get_bins_maps", "GBDTSingle",
-           "GBDTMulti", "GBDTMO_classifier", "GBDTMO_regression"]
+           "GBDTMulti",
+           "GBDTMulti_regression",
+           "GBDTMulti_classification"]
